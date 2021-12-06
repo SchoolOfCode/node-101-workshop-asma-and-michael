023093156838d8aff610daab9053205dbf6544e1
AsmaDage <asmadage@gmail.com>
2021-12-06 17:22:36 +0000

    Typo in task 2.c fixed. Task 2.d remains outstanding.

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ let myCollection = [
 function describeItem(item) {
   if (item.count === 1) {
     console.log(
-      `I have a ${item.name} Here's what I like about it: ${item.whatILike}`
+      `I have a ${item.name} Here's what I like about it: ${item.whatILike}.`
     );
   } else {
     console.log(
-      `I have ${item.count} ${names}. Here's what I like about them: ${item.whatILike}`
+      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}.`
     );
   }
 }
